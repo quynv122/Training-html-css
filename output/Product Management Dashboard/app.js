3e42b7ccb3ea2feb6e4b9837a270de0a2220281d
Quy <quynv.122@gmail.com>
2025-10-11 17:04:01 +0700

    hoàn thành: Dashboard quản lý sản phẩm - CRUD, tìm kiếm, sắp xếp, phân trang

--- output/Product Management Dashboard/app.js
+++ output/Product Management Dashboard/app.js
@@ -1,3 +1,4 @@
+// trạng thái mặc dịnh của page
 const rowPerPage = 5;
 let currentPage = 1;
 let currentKeyword = "";
@@ -5,6 +6,7 @@ let currentPriceSort = "";
 
 renderTable(getProducts());
 
+// hàm render dữ liệu bảng
 function renderTable(
   data,
   currentPage = 1,
@@ -12,8 +14,8 @@ function renderTable(
   totalRow = data.length
 ) {
   // vô hiệu hóa nút chuyển trang
-  document.querySelector(".pagination__btn--next").disabled = true;
-  document.querySelector(".pagination__btn--prev").disabled = true;
+  document.querySelector(".pagination__button--next").disabled = true;
+  document.querySelector(".pagination__button--prev").disabled = true;
 
   //làm trống dữ liệu bảng
   const tbody = document.querySelector("tbody");
@@ -29,35 +31,53 @@ function renderTable(
     currentPage * rowPerPage
   );
 
-  // nếu không có sản phẩm
+  // kiểm tra sô lượng sản phảm
   if (renderData.length === 0) {
     const tr = document.createElement("tr");
-    tr.innerHTML = `<td colspan=6 class="table__cell table__cell--blank">No products!</td>`;
+    tr.innerHTML = `<td colspan=6 class="table__cell table__cell--empty">No products!</td>`;
     tbody.appendChild(tr);
   } else {
+    // hiển thị keyword nên iput search
+    document.querySelector(".search__input").value = currentKeyword;
+
+    // kiểm tra, hiển thị icon sort
+    const sortIcon = document.querySelector(".table__cell__sort-icon");
+    sortIcon.classList.remove("asc", "desc");
+    if (currentPriceSort === "asc") {
+      sortIcon.classList.add("asc");
+    }
+    if (currentPriceSort === "desc") {
+      sortIcon.classList.add("desc");
+    }
+
+    // render dữ liệu các row
     renderData.forEach((product, index) => {
       const tr = document.createElement("tr");
       tr.classList.add("table__row");
       tr.setAttribute("data-id", product.id);
       tr.innerHTML += ` 
-                    <td class="table__cell">${index + 1}</td>
-                    <td class="table__cell">${product.name}</td>
+                    <td class="table__cell table__cell--number">${index + 1}</td>
+                    <td class="table__cell table__cell--name" 
+                    title = "${product.name}"
+                    >${product.name}</td>
                     
-                    <td class="table__cell">${formatVietnameseCurrency(
+                    <td class="table__cell table__cell--price">${formatVietnameseCurrency(
                       Number(product.price) // chuyển số sang tiền vnd
                     )}</td>
-                    <td class="table__cell">
-                        <span class="status-badge ${product.status}">${
-        product.status
-      }</span>
+                    <td class="table__cell table__cell--status">
+                        <span class="status-badge status-badge--${product.status}">${product.status}</span>
                     </td>
                     <td class="table__cell table__cell--actions">
-                        <div  class="table__action-btn table__action-btn--edit">
-                            Edit
-                        </div>
-                        <div  class="table__action-btn table__action-btn--delete">
-                            Delete
-                        </div>
+                         <div class="actions">
+                                <button class="actions__button actions__button--edit"
+                                title="Edit product">
+                                   Edit
+                                </button>
+                                <button class="actions__button actions__button--delete"
+                                title="Delete product">
+                                    Delete
+                                </button>
+                            </div>
                     </td>`;
 
       tbody.appendChild(tr);
@@ -69,43 +89,28 @@ function renderTable(
   document.getElementById("current-page").textContent = currentPage;
   document.getElementById("total-page").textContent = totalPage;
 
-  // hiển thị keyword nên iput search nếu đang tìm kiếm
-  document.querySelector(".table__search-input").value = currentKeyword;
-
-  // kiểm tra, hiển thị icon sort
-  const sortIcon = document.querySelector(".sort-icon");
-  sortIcon.classList.remove("asc", "desc");
-
-  if (currentPriceSort === "asc") {
-    sortIcon.classList.add("asc");
-  }
-  if (currentPriceSort === "desc") {
-    sortIcon.classList.add("desc");
-  }
-
   // kiểm tra dữ liệu để active nút chuyển trang
   if (currentPage < totalPage) {
-    document.querySelector(".pagination__btn--next").disabled = false;
+    document.querySelector(".pagination__button--next").disabled = false;
   }
   if (currentPage > 1) {
-    document.querySelector(".pagination__btn--prev").disabled = false;
+    document.querySelector(".pagination__button--prev").disabled = false;
   }
 }
 
-// xóa sản phẩm
+// hàm xóa sản phẩm
 function deleteProduct(id) {
-  // mở popup comfirm, set nội dung cho popup
-  const popupComfirm = document.querySelector(".popup-comfirm");
-  const popupTitle = document.querySelector(".popup-comfirm__title");
-  const popupMess = document.querySelector(".popup-comfirm__mess");
-  const popupBtnAgree = document.querySelector(".popup-comfirm__btn--agree");
-  const popupBtnCancel = document.querySelector(".popup-comfirm__btn--cancel");
+  const popupComfirm = document.querySelector(".confirm-popup");
+  const popupTitle = document.querySelector(".confirm-popup__title");
+  const popupMess = document.querySelector(".confirm-popup__message");
+  const popupBtnAgree = document.querySelector(".confirm-popup__button--agree");
+  const popupBtnCancel = document.querySelector(".confirm-popup__button--cancel");
 
+  // mở popup, set nội dung cho popup
   popupTitle.textContent = "CONFIRM PRODUCT DELETION";
   popupMess.textContent = "Are you sure you want to delete this product?";
   popupBtnAgree.textContent = "Delete Product";
   popupBtnCancel.textContent = "Cancel";
-
   popupComfirm.classList.add("active");
 
   // gắn EventListener cho 2 nút agree, cancel của popup
@@ -120,10 +125,10 @@ function deleteProduct(id) {
   // hàm xử lý khi đống ý
   function handleAgree() {
     let products = getProducts(); // lấy dữ liệu từ localstorage
-    let newProducts = products.filter((product) => product.id !== id); // lọc bỏ phần tử theo id
+    let newProducts = products.filter((product) => product.id !== id); // loại bỏ phần tử theo id
     saveProducts(newProducts); // lưu dữ liệu mới vào localstorage
-    updateView(); // render table
-    closePopup(); // đóng modal,  clear EventListener
+    refreshTable(); // render table
+    closePopup(); // đóng popup,  clear EventListener
     showToast("success", "Product deleted"); // hien thị thông báo
   }
 
@@ -137,12 +142,11 @@ function deleteProduct(id) {
 
 // hàm thêm sản phẩm
 function addProduct() {
-  // mở modal,form + set nội dung
   const modal = document.querySelector(".modal");
   const modalTitle = document.querySelector(".modal__title");
-  const submitBtn = document.querySelector(".modal__btn--submit");
-  const cancelBtn = document.querySelector(".modal__btn--cancel");
-
+  const submitBtn = document.querySelector(".modal__button--submit");
+  const cancelBtn = document.querySelector(".modal__button--cancel");
+  // mở modal,form + set nội dung
   modalTitle.textContent = "Add Product";
   submitBtn.textContent = "Add";
   cancelBtn.textContent = "Cancel";
@@ -166,24 +170,24 @@ function addProduct() {
 
     // validate và hiển thị err
     if (productName.value.trim() === "") {
-      document.querySelector(".modal__error--name").textContent =
+      document.querySelector(".form-field__error--name").textContent =
         "Please enter the product name.";
       valid = false;
-    } else if (productName.value.trim().length > 50) {
-      document.querySelector(".modal__error--name").textContent =
+    } else if (productName.value.trim().length > 150) {
+      document.querySelector(".form-field__error--name").textContent =
         "Product name is too long.";
       valid = false;
     }
     if (productPrice.value === "") {
-      document.querySelector(".modal__error--price").textContent =
+      document.querySelector(".form-field__error--price").textContent =
         "Please enter the product price.";
       valid = false;
     } else if (isNaN(productPrice.value) || productPrice.value <= 0) {
-      document.querySelector(".modal__error--price").textContent =
+      document.querySelector(".form-field__error--price").textContent =
         "The product price must be a number greater than 0.";
       valid = false;
-    } else if (productPrice.value.length > 9) {
-      document.querySelector(".modal__error--price").textContent =
+    } else if (productPrice.value.length > 12) {
+      document.querySelector(".form-field__error--price").textContent =
         "Product price is too high.";
       valid = false;
     }
@@ -209,27 +213,23 @@ function addProduct() {
 
       // xóa dữ liệu input
       document.querySelector(".modal__form").reset();
-      showToast("success", "Product added successfully."); // hiển thị thông báo
+      showToast("success", "Product added successfully.");
       handleClose();
     }
   }
 
   // hàm xử lý khi hủy
   function handleClose() {
-    closeModal(); // đóng modal, clear EventListener
-    clearError(); // xóa error
-  }
-
-  // hàm đóng modal + clear EventListener
-  function closeModal() {
+    // đóng modal, clear EventListener
     modal.classList.remove("active");
     submitBtn.removeEventListener("click", handleAgree);
     cancelBtn.removeEventListener("click", handleClose);
+    clearError(); // xóa error
   }
 
   // hàm clear err
   function clearError() {
-    document.querySelectorAll(".modal__error").forEach((e) => {
+    document.querySelectorAll(".form-field__error").forEach((e) => {
       e.textContent = "";
     });
   }
@@ -247,7 +247,7 @@ function editProduct(id) {
     currentPage = 1;
     currentKeyword = "";
     currentPriceSort = "";
-    updateView();
+    refreshTable();
     return;
   }
 
@@ -259,8 +259,8 @@ function editProduct(id) {
 
   const modal = document.querySelector(".modal");
   const modalTitle = document.querySelector(".modal__title");
-  const cancelBtn = document.querySelector(".modal__btn--cancel");
-  const saveBtn = document.querySelector(".modal__btn--submit");
+  const cancelBtn = document.querySelector(".modal__button--cancel");
+  const saveBtn = document.querySelector(".modal__button--submit");
   const inputName = document.getElementById("productName");
   const inputPrice = document.getElementById("productPrice");
   const inputStatus = document.getElementById("productStatus");
@@ -304,24 +304,24 @@ function editProduct(id) {
 
     // Validate dữ liệu
     if (newName === "") {
-      document.querySelector(".modal__error--name").textContent =
+      document.querySelector(".form-field__error--name").textContent =
         "Please enter the product name.";
       valid = false;
-    } else if (newName.length > 50) {
-      document.querySelector(".modal__error--name").textContent =
+    } else if (newName.length > 150) {
+      document.querySelector(".form-field__error--name").textContent =
         "Product name is too long.";
       valid = false;
     }
     if (newPrice === "") {
-      document.querySelector(".modal__error--price").textContent =
+      document.querySelector(".form-field__error--price").textContent =
         "Please enter the product price.";
       valid = false;
     } else if (isNaN(newPrice) || Number(newPrice) <= 0) {
-      document.querySelector(".modal__error--price").textContent =
+      document.querySelector(".form-field__error--price").textContent =
         "The product price must be a number greater than 0.";
       valid = false;
-    } else if (newPrice.length > 9) {
-      document.querySelector(".modal__error--price").textContent =
+    } else if (newPrice.length > 12) {
+      document.querySelector(".form-field__error--price").textContent =
         "Product price is too high.";
       valid = false;
     }
@@ -340,22 +340,22 @@ function editProduct(id) {
     }
 
     // Cập nhật thông tin sanr phẩm
-    const newProducts = products.map((p) =>
-      p.id === id
+    const newProducts = products.map((product) =>
+      product.id === id
         ? {
-            ...p,
+            ...product,
             name: newName,
             price: newPrice,
             status: newStatus,
             description: newDescription,
           }
-        : p
+        : product
     );
 
     saveProducts(newProducts);
     currentPriceSort = "";
     currentPage = 1;
-    updateView();
+    refreshTable();
     handleClose();
     showToast("success", "Product updated successfully!");
   }
@@ -363,7 +363,7 @@ function editProduct(id) {
   // hàm xóa err
   function clearError() {
     document
-      .querySelectorAll(".modal__error")
+      .querySelectorAll(".form-field__error")
       .forEach((e) => (e.textContent = ""));
   }
 }
@@ -385,12 +385,6 @@ function genID() {
   return id;
 }
 
-// vô hiệu hóa sự kiện mặc định của form
-const form = document.querySelector("form");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
-
 // hàm đổi số sang tiền (vnd)
 function formatVietnameseCurrency(number) {
   if (isNaN(number)) return "0 ₫";
@@ -402,26 +396,24 @@ function formatVietnameseCurrency(number) {
 // hàm hiernr thị thông báo, toast
 function showToast(type = "", mess = "") {
   const toast = document.querySelector(".toast");
-  const toastMsg = document.createElement("p");
 
-  toastMsg.classList.add("toast__mess");
-  toastMsg.textContent = mess;
-
-  toast.replaceChildren();
+  toast.textContent = '';
   toast.classList.remove("toast--success", "toast--error", "toast--warning");
+  toast.classList.remove("active");
+
   if (type) toast.classList.add(`toast--${type}`);
 
-  toast.appendChild(toastMsg);
+  toast.textContent = mess;
   toast.classList.add("active");
 
+
   setTimeout(() => {
-    toastMsg.remove();
     toast.classList.remove("active");
   }, 3000);
 }
 
 // hàm xử lý dữ liệu trước khi render table
-function updateView() {
+function refreshTable() {
   let products = getProducts();
 
   // kiểm tra trạng thái hiện tại của table
@@ -452,38 +444,7 @@ function updateView() {
   }
 }
 
-// tìm kiếm theo tên
-document.querySelector(".table__search-btn").addEventListener("click", () => {
-  let keyword = document.querySelector(".table__search-input").value;
-  if (keyword.trim() !== "") {
-    currentKeyword = keyword;
-    currentPage = 1;
-    currentPriceSort = "";
-    updateView();
-  }
-});
 
-// bắt sự kiện click sắp xếp
-document.querySelector(".table__cell--sortable").addEventListener("click", () => {
-  // chuyển đổi giữa 3 trạng thái sắp xếp: "", "asc", "desc"
-    switch (currentPriceSort) {
-      case "":
-        currentPriceSort = "asc";
-        currentPage = 1;
-        updateView();
-        break;
-      case "asc":
-        currentPriceSort = "desc";
-        currentPage = 1;
-        updateView();
-        break;
-      case "desc":
-        currentPriceSort = "";
-        currentPage = 1;
-        updateView();
-        break;
-    }
-  });
 
 // hàm sắp xếp dữ liệu sản phẩm.
 function sortProductsByPrice(products, order = "asc") {
@@ -492,47 +453,9 @@ function sortProductsByPrice(products, order = "asc") {
     else return b.price - a.price;
   });
 }
-// bắt sự kiện nút chuyển trang tiếp,
-const btnNextPage = document.querySelector(".pagination__btn--next");
-btnNextPage.addEventListener("click", () => {
-  currentPage++;
-  updateView();
-});
 
-// bắt sự kiện nút chuyển trang trước,
-const btnPrevPage = document.querySelector(".pagination__btn--prev");
-btnPrevPage.addEventListener("click", () => {
-  currentPage--;
-  if (currentPage === 0) {
-    showToast("error", "Something went wrong.");
-    currentPage = 1;
-    updateView();
-  } else {
-    updateView();
-  }
-});
 
-//bắt sự kiện click trên tbody chứa các sản phẩm
-const tbody = document.querySelector(".table__body");
-tbody.addEventListener("click", (e) => {
-  // kiểm tra nếu nhấn vào nút xóa sản phẩm
-  if (e.target.classList.contains("table__action-btn--delete")) {
-    // gọi đến thẻ cha(tr) của sản phẩm đó. lấy ra id sản phẩm dó (id đc gắn vào attribute khi render table)
-    const idProduct = e.target.closest(".table__row").getAttribute("data-id");
-
-    //gọi hàm xóa sản phẩm
-    deleteProduct(idProduct);
-  }
-  if (e.target.classList.contains("table__action-btn--edit")) {
-    // gọi đến thẻ cha(tr) của sản phẩm đó. lấy ra id sản phẩm dó (id đc gắn vào attribute khi render table)
-    const idProduct = e.target.closest(".table__row").getAttribute("data-id");
-
-    //gọi hàm sửa sản phẩm
-    editProduct(idProduct);
-  }
-});
-
-// đổi số sang tiền việt
+// hàm chuyển chữ tiếng việt thành chũ latin
 function removeVietnameseTones(str) {
   return str
     .normalize("NFD")
@@ -551,9 +474,99 @@ function searchProducts(products, keyword) {
   );
 }
 
-document.querySelector(".table__btn-reset").addEventListener("click", () => {
+// vô hiệu hóa sự kiện mặc định của form
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+
+// bắt sự kiện nút reset table
+document.querySelector(".button--reset").addEventListener("click", () => {
   currentKeyword = "";
   currentPage = 1;
   currentPriceSort = "";
   renderTable(getProducts());
 });
+
+
+
+//bắt sự kiện click trên tbody 
+const tbody = document.querySelector(".table__body");
+tbody.addEventListener("click", (e) => {
+  const deleteBtn = e.target.closest(".actions__button--delete");
+  const editBtn = e.target.closest(".actions__button--edit");
+
+  // Kiểm tra nếu nhấn vào nút xóa
+  if (deleteBtn) {
+    const idProduct = deleteBtn.closest(".table__row").getAttribute("data-id"); 
+    deleteProduct(idProduct);
+  }
+
+  // nếu nhấn vào nút edit
+  if (editBtn) {
+    const idProduct = editBtn.closest(".table__row").getAttribute("data-id");
+    editProduct(idProduct);
+  }
+});
+
+// bắt sự kiện nút chuyển trang tiếp,
+const btnNextPage = document.querySelector(".pagination__button--next");
+btnNextPage.addEventListener("click", () => {
+  currentPage++;
+  refreshTable();
+});
+
+// bắt sự kiện nút chuyển trang trước,
+const btnPrevPage = document.querySelector(".pagination__button--prev");
+btnPrevPage.addEventListener("click", () => {
+  currentPage--;
+  // kiểm tra current page
+  if (currentPage < 1) {
+    showToast("error", "Something went wrong.");
+    currentPage = 1;
+    refreshTable();
+  } else {
+    refreshTable();
+  }
+});
+
+// bắt sự kiện nút search
+document.querySelector(".search__button").addEventListener("click", () => {
+  let keyword = document.querySelector(".search__input").value;
+  // kiểm tra keyword
+  if (keyword.trim() !== "") {
+    currentKeyword = keyword;
+    currentPage = 1;
+    currentPriceSort = "";
+    refreshTable();
+  }
+});
+
+
+// bắt sự kiện click sắp xếp
+document
+  .querySelector(".table__cell--price")
+  .addEventListener("click", () => {
+    // chuyển đổi giữa 3 trạng thái sắp xếp: "", "asc", "desc"
+    switch (currentPriceSort) {
+      case "":
+        currentPriceSort = "asc";
+        currentPage = 1;
+        refreshTable();
+        break;
+      case "asc":
+        currentPriceSort = "desc";
+        currentPage = 1;
+        refreshTable();
+        break;
+      case "desc":
+        currentPriceSort = "";
+        currentPage = 1;
+        refreshTable();
+        break;
+    }
+  });
+  
+  // bắt sự kiện click nút add product
+  document.querySelector('.button--add').addEventListener('click',() => addProduct())
